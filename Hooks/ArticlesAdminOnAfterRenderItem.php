6a56f596acff7d8b4e23e38f2ee86401cba47d49
Dhens <rudenyl@gmail.com>
2016-07-05 18:15:33 +0800

    Use startPush as base layout using stack directive.

--- Hooks/ArticlesAdminOnAfterRenderItem.php
+++ Hooks/ArticlesAdminOnAfterRenderItem.php
@@ -8,6 +8,6 @@ class ArticlesAdminOnAfterRenderItem
     public function handle($view)
     {
         $buffer = (new Stub(__DIR__ . '/../Stubs/admin.summernote.stub', []))->render();
-        $view->getFactory()->startSection('header', $buffer);
+        $view->getFactory()->startPush('header', $buffer);
     }   
 }
